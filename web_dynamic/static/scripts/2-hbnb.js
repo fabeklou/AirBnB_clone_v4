9b81bf8a92a4ef2798532dc81c6b83865606b565
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-04-06 10:29:30 +0000

    fix: semistandard multiple space warning

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -21,11 +21,10 @@ $(document).ready(function () {
 
   $.ajax({
     type: 'GET',
-    url: 'http://localhost:5001/api/v1/status/',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
     success: function (res) {
       if (res.status === 'OK') $('div#api_status').addClass('available');
-      else  $('div#api_status').removeClass('available');
+      else $('div#api_status').removeClass('available');
     }
   });
-
 });
